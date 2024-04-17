7a3e681a0e731afaa151062de676bfe05cde37ea
ballo1988 <rounderjards@gmail.com>
2024-04-17 15:45:26 -0600

    removed unnecesary links

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -17,7 +17,6 @@ export default function Cards() {
                         Some quick example text to build on the card title and make up the
                         bulk of the card's content.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
@@ -28,7 +27,6 @@ export default function Cards() {
                         Some quick example text to build on the card title and make up the
                         bulk of the card's content.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
@@ -39,7 +37,6 @@ export default function Cards() {
                         Some quick example text to build on the card title and make up the
                         bulk of the card's content.
                     </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
             </Card>
             <Card style={{ width: '18rem' }}>
